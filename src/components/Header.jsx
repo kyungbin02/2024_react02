8ffd8a0ea584aadfedfbefd074d5fe79a25a5543
kyungbin <thrudqls13@naver.com>
2024-11-20 17:52:21 +0900

    11/20-오후

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import LoginBody from "./components/LoginBody";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <LoginBody />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
--- src/components/step04/TodoList.jsx
+++ src/components/step04/TodoList.jsx
@@ -2,11 +2,24 @@ import React, { useState } from 'react';
 
 function TodoList(props) {
     const [todos, setTodos] = useState(['Learn React', 'Build a Project'])
+    const [newTodo, setNewTodo] = useState('')
+
+
+    // 리스트에서 삭제
     const deleteTodo = (index) => {
         // (_, i) => fillter메서드의 콜백 함수 매개변수 
         // 첫번째 매개변수 _ 는 배열의 각 요소를 의미(지금은 사용하지 않으므로 _ )
         // 두번째 매개변수 i 는 인텍스를 의미한다.
         setTodos(todos.filter((_, i) => i !== index));
+    }
+
+    // 삽입
+    const addTodo = () => {
+        if (newTodo.trim !== '') {
+            // 기존 todos  배열에 새로운 정보를 가진 newTodo를 추가한다.
+            setTodos([...todos, newTodo.trim()])
+            setNewTodo('');
+        }
     }
     return (
         <div>
@@ -23,13 +36,13 @@ function TodoList(props) {
                     </li>
                 ))}
             </ul>
-            <p>Typing : </p>
+            <p>Typing : {newTodo} </p>
             <input
                 type='text'
-                value
-                onChange
-            />{''}
-            <button onClick >Add New Task</button>
+                value={newTodo}
+                onChange={(e)=>setNewTodo(e.target.value)}
+            />{'   '}
+            <button onClick={addTodo} >Add New Task</button>
         </div>
     );
 }
